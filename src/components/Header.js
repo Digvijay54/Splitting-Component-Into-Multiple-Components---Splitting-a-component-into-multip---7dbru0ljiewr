22a7daed1fb57272c0a905e04960a552cbb91b79
Digvijay <digvijaykumar54@gmail.com>
2023-02-23 16:50:44 +0530

    Main

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,10 @@
+import React from "react";
 
+const Header=(props)=>{
+    return(
+        <div id="header">
+            <header id={props.id}>{props.content}</header>
+        </div>
+    );
+}
+export default Header;
