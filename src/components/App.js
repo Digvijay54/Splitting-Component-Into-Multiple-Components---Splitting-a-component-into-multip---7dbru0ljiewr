39d898fed47d08e1e296ee6135ec171b9b70948a
Digvijay <digvijaykumar54@gmail.com>
2023-02-23 16:51:01 +0530

    Main

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,10 @@ import Main from "./Main";
 import Footer from "./Footer";
 function App() {
     return (
-        <div className="container">
-            <header id="my-header">This is the header</header>
-            <main id="my-main">This is the main</main>
-            <footer id="my-footer">This is the footer</footer>
+        <div id="container">
+            <header id="my-header" content="This is the header" />
+            <main id="my-main" content="This is the main" />
+            <footer id="my-footer" content="This is the footer" />
         </div>
     );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,1 +1,10 @@
+import React from "react";
 
+const Main=(props)=>{
+    return(
+        <div id="main">
+            <main id={props.id}>{props.content}</main>
+        </div>
+    );
+}
+export default Main;
